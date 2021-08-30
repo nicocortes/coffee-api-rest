aefc6f34feb7cd8e0132aab609c0682cfe491087
Nico Cortes <ncortescarbajo@gmail.com>
2021-08-30 08:51:32 -0300

    Validaciones

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -10,15 +10,39 @@ const {
 	usuariosDelete,
 } = require("../controllers/usuarios");
 
+//Controladores
+const { validarCampos } = require("../middlewares/validarCampos");
+const { emailExiste, idExiste } = require("../helpers/dbValidators");
+
 router.get("/", usuariosGet);
 
 router.post(
 	"/",
-	[check("nombre", "El nombre es obligatorio").not().isEmpty()],
+	[
+		check("nombre", "El nombre es obligatorio").not().isEmpty(),
+		check("password", "Debe tener una contraseña").not().isEmpty().trim(),
+		check(
+			"password",
+			"La contraseña debe tener 5 caracteres como mínimo"
+		).isLength({ min: 6 }),
+		check("email", "No es un correo válido. ").isEmail(),
+		check("email").custom(emailExiste),
+		check("rol", "No es un rol válido").isIn(["USER_ROLE", "ADMIN_ROLE"]),
+		validarCampos,
+	],
 	usuariosPost
 );
 
-router.put("/:id", usuariosPut);
+router.put(
+	"/:id",
+	[
+		check("id", "No es un Id válido").isMongoId(),
+		[check("id").custom(idExiste)],
+		validarCampos,
+	],
+
+	usuariosPut
+);
 
 router.delete("/:id", usuariosDelete);
 
